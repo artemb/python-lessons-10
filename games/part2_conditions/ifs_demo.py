502541002d7de722df6579a0757d8e0a8a4e1e7c
artemb <a.bobrovskij@gmail.com>
2021-02-25 15:53:26 +0000

    Update

---
+++ games/part2_conditions/ifs_demo.py
@@ -0,0 +1,26 @@
+from random import randint
+
+# Example of an IF
+
+# General form
+'''
+if <condition>:
+    <body>
+
+'''
+# Indentation for blocks of code
+
+# Boolean values
+
+# Comparison operators
+
+# Logical operators
+
+# pass
+
+# else
+
+# elif
+
+# nested ifs
+
