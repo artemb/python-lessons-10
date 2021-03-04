f138d9dc6408aa3ffd6e73f16e9cfc67b735464a
artemb <a.bobrovskij@gmail.com>
2021-03-04 16:22:05 +0000

    Update

--- games/part2_conditions/ifs_demo.py
+++ games/part2_conditions/ifs_demo.py
@@ -14,13 +14,64 @@ if <condition>:
 
 # Comparison operators
 
+# a = 60
+# b = 50
+#
+# print(a == b)
+# print(a > 50)
+# print(a < 50)
+# print(b >= 50)
+# print(b <= 50)
+# print(a != b)
+
 # Logical operators
+
+a = 150
+
+if a > 0 and a < 100:
+    print('A is between 0 and 100')
+
+if a > 100 or a < 0:
+    print('Hello')
+
+# True and True = True
+# False and False = False
+# True and False = False
+# False and True = False
+
+# True or True = True
+# True or False = True
+# False or True = True
+# False or False = False
+
 
 # pass
 
 # else
 
+x = 5
+y = 100
+
+if x > 10:
+    print('X is greater than 10')
+else:
+    print('X is not greater than 10')
+
+if y < 50:
+    print("Y is less than 50")
+else:
+    print('Y is not less than 50')
+
 # elif
+
+a = -14.5
+
+if a < 10:
+    print('A is less than 10')
+elif a > 100:
+    print('A is greater than 100')
+else:
+    print('A is between 10 and 100')
 
 # nested ifs
 
